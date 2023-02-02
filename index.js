fcf2eaed3fb915da90984e7123824321a72ac991
ChalCutiya <115392064+ChalCutiya@users.noreply.github.com>
2023-02-02 11:30:32 +0000

    third

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ const { json } = require('body-parser');
 const express = require('express');
 const credential = require('./app/controllers/crd_manage')
 
+const port = process.env.port || 3000;
 
-const cors =require('cors');
+const cors = require('cors');
+const { prototype } = require('events');
 const app = express()
 app.use(express.json());
 app.use(cors({
@@ -11,38 +13,38 @@ app.use(cors({
     "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
     "preflightContinue": false,
     "optionsSuccessStatus": 204
-  }))
+}))
 
 
 // this is for get all the data
 
 app.get('', async (req, resp) => {
-    allcds=await credential.getcds();
-   resp.send(allcds);
+    allcds = await credential.getcds();
+    resp.send(allcds);
 })
 
-    //// Creating a New User /////
+//// Creating a New User /////
 app.post('/newuser', async (req, resp) => {
     console.log(req.body);
-    if (req.body.username.length == null || req.body.password == null || (req.body.username.length < 4 )|| (req.body.password.length < 4))
+    if (req.body.username.length == null || req.body.password == null || (req.body.username.length < 4) || (req.body.password.length < 4))
         resp.send("New User is not Created \nUserName or Password not Valid (minimum lenght should be 4)");
     else {
-        const result = await credential.setcds(req.body.username,req.body.password);
+        const result = await credential.setcds(req.body.username, req.body.password);
         resp.send(result);
     }
 })
 
 app.post('/login', async (req, resp) => {
     console.log(req.body);
-    if (req.body.username.length == null || req.body.password == null || (req.body.username.length < 4 )|| (req.body.password.length < 4))
-        resp.send("Invalid Cridentials",401);
+    if (req.body.username.length == null || req.body.password == null || (req.body.username.length < 4) || (req.body.password.length < 4))
+        resp.send("Invalid Cridentials", 401);
     else {
-        const result = await credential.verifycds(req.body.username,req.body.password);
-        resp.send(result.message,result.sc);
+        const result = await credential.verifycds(req.body.username, req.body.password);
+        resp.send(result.message, result.sc);
     }
 })
 
 
-app.listen(5000,()=>{
-    console.log("listending on the 5000 port");
+app.listen(port, () => {
+    console.log(`app is listening on port: ${port}`);
 });
